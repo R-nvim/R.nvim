e9c629bb4e405aebfe4ec8ad61b667a2463d75a5
Jakson Alves de Aquino <jalvesaq@gmail.com>
2024-01-28 21:52:43 -0300

    Convert ftplugin/quarto_rnvim.vim to Lua

---
+++ ftplugin/quarto_rnvim.lua
@@ -0,0 +1,11 @@
+if vim.fn.exists("g:R_filetypes") == 1 and type(vim.g.R_filetypes) == "table" and vim.fn.index(vim.g.R_filetypes, 'quarto') == -1 then
+    return
+end
+
+require("r.config").real_setup()
+require("r.rmd").setup()
+
+local m = require("r.maps")
+m.create('n', 'RQuartoRender',  'qr', ':lua require("r.quarto").cmd("render")')
+m.create('n', 'RQuartoPreview', 'qp', ':lua require("r.quarto").cmd("preview")')
+m.create('n', 'RQuartoStop',    'qs', ':lua require("r.quarto").cmd("stop")')
