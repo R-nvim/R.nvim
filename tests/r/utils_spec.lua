3f5788f4dcb2cb626ab56780e73262c83e5c64f4
Jakson Alves de Aquino <jalvesaq@gmail.com>
2024-02-05 21:27:05 -0300

    Fix test

--- tests/r/utils_spec.lua
+++ tests/r/utils_spec.lua
@@ -3,8 +3,8 @@ local utils = require("r.utils")
 
 --- See tests/init/lua
 local function root(root)
-  local f = debug.getinfo(1, "S").source:sub(2)
-  return vim.fn.fnamemodify(f, ":p:h:h") .. "/" .. (root or "")
+    local f = debug.getinfo(1, "S").source:sub(2)
+    return vim.fn.fnamemodify(f, ":p:h:h") .. "/" .. (root or "")
 end
 
 describe("normalize_windows_path", function()
@@ -14,8 +14,6 @@ describe("normalize_windows_path", function()
         assert.are.equal(expected, utils.normalize_windows_path(path))
     end)
 end)
-
-
 
 -- TODO: Add tests for Windows
 -- TODO: Add tests for Mac
@@ -27,19 +25,20 @@ describe("ensure_directory_exists", function()
         assert.is_true(utils.ensure_directory_exists(existing_directory))
     end)
 
-    it("should create the directory and return true if it does not exist (Unix)", function()
-        local new_directory = test_cache_directory .. "/new-unix-directory"
-        assert.is_true(utils.ensure_directory_exists(new_directory))
-        os.execute("rm -r " .. new_directory)  -- Clean up: Remove the created directory
-    end)
+    it(
+        "should create the directory and return true if it does not exist (Unix)",
+        function()
+            local new_directory = test_cache_directory .. "/new-unix-directory"
+            assert.is_true(utils.ensure_directory_exists(new_directory))
+            os.execute("rm -r " .. new_directory) -- Clean up: Remove the created directory
+        end
+    )
 
-    --FIXME: should work but doesn't. maybe I don't understand busted.
-    -- it("should return false and an error message if an error occurs during directory creation (Unix)", function()
-    --     local protectedDir = "/usr/bin"  -- Attempt to create a directory in a protected Unix directory
-    --     local success, errorMessage = utils.ensure_directory_exists(protectedDir)
-    --     assert.is_false(success)
-    --     assert.is_not_nil(errorMessage)
-    -- end)
-
+    it(
+        "should return false if an error occurs during directory creation (Unix)",
+        function()
+            local protectedDir = "/create_me" -- Attempt to create a directory in a protected Unix directory
+            assert.is_false(utils.ensure_directory_exists(protectedDir))
+        end
+    )
 end)
-
