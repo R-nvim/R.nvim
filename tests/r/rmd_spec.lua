64c237e250cb0a2f5fe6d0045ac29cdf81c252b3
Anas Sheashaey <she3sha3y5@gmail.com>
2024-02-26 01:29:57 +0200

    Add pending tests to `rmd.write_chunk()`

--- tests/r/rmd_spec.lua
+++ tests/r/rmd_spec.lua
@@ -9,6 +9,12 @@ describe("rmd module", function()
     local function send_lua_via_rpc(lua_command, args)
         return rpcrequest(nvim, "nvim_exec_lua", lua_command, args or {})
     end
+    local function set_filetype(filetype)
+        rpcrequest(nvim, "nvim_buf_set_option", 0, "filetype", filetype)
+    end
+    local function get_current_line()
+        return rpcrequest(nvim, "nvim_win_get_cursor", 0)[2]
+    end
 
     before_each(
         function()
@@ -49,7 +55,7 @@ describe("rmd module", function()
             assert.is_false(result)
         end)
         it("returns true if on an '```{r' line", function()
-            rpcrequest(nvim, "nvim_win_set_cursor", 0, { 12, 0 })
+            rpcrequest(nvim, "nvim_win_set_cursor", 0, { 12, 3 })
             local result =
                 send_lua_via_rpc([[return require('r.rmd').is_in_code_chunk('r', false)]])
             assert.is_true(result)
@@ -67,5 +73,32 @@ describe("rmd module", function()
             "returns false if it cannot match ```",
             function() pending("test behaviour if chunk start/end not found") end
         )
+    end)
+    describe("write_chunk() Unit Tests", function()
+        it(
+            "Inserts an R code chunk in an empty line for RMarkdown",
+            function() pending("Not implemented") end
+        )
+        it(
+            "Inserts an R code chunk in an empty line for Quarto",
+            function() pending("Not implemented") end
+        )
+        it(
+            "Does not insert an R code chunk in a non-empty line in RMarkdown",
+            function() pending("Not implemented") end
+        )
+        it(
+            "Does not insert an R code chunk in a non-empty line in Quarto",
+            function() pending("Not implemented") end
+        )
+        it(
+            "Does not insert an R code chunk in a code chunk line in RMarkdown",
+            function() pending("Not implemented") end
+        )
+        it(
+            "Does not insert an R code chunk in a code chunk in Quarto",
+            function() pending("Not implemented") end
+        )
+
     end)
 end)
