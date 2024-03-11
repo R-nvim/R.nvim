c134452bfe71be7938e06c68af85fbc9878e5431
Jakson Alves de Aquino <jalvesaq@gmail.com>
2024-03-11 16:36:09 -0300

    Skip 00LOCK directory
    Close #81

--- nvimcom/src/apps/rnvimserver.c
+++ nvimcom/src/apps/rnvimserver.c
@@ -1094,6 +1094,11 @@ void update_inst_libs(void) {
                     }
                     if (r)
                         continue;
+
+                    // Skip failed installations
+                    if (strstr(dir->d_name, "00LOCK"))
+                        continue;
+
                     snprintf(fname, 511, "%s/%s/DESCRIPTION", lp->path,
                              dir->d_name);
                     descr = read_file(fname, 1);
