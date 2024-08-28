65ed8c5d558f2aa7f622d6754a4fffc985dcdad7
Philippe Massicotte <pmassicotte@hotmail.com>
2024-08-28 17:04:11 -0400

    style(config.lua): reformat long lines for better readability feat(config.lua): add Roxygenize user command to insert roxygen skeleton style(maps.lua): remove redundant RInsertRoxygen mapping

--- lua/r/maps.lua
+++ lua/r/maps.lua
@@ -21,7 +21,6 @@ local map_desc = {
     RSeparatePath       = { m = "", k = "", c = "Edit",     d = "Split the file path or the url under the cursor" },
     RFormatNumbers      = { m = "", k = "", c = "Edit",     d = "Add an 'L' suffix after numbers to explicitly indicate them as integers" },
     RFormatSubsetting   = { m = "", k = "", c = "Edit",     d = "Replace all the `$` subsetting operators with `[[` in the current buffer" },
-    RInsertRoxygen      = { m = "", k = "", c = "Edit",     d = "Insert roxygen skeleton above the current function definition" },
     RNextRChunk         = { m = "", k = "", c = "Navigate", d = "Go to the next chunk of R code" },
     RGoToTeX            = { m = "", k = "", c = "Navigate", d = "Go the corresponding line in the generated LaTeX document" },
     RPreviousRChunk     = { m = "", k = "", c = "Navigate", d = "Go to the previous chunk of R code" },
@@ -220,7 +219,6 @@ local edit = function()
     -- Format functions
     create_maps("nvi", "RFormatNumbers",    "cn", "<Cmd>lua require('r.format.numbers').formatnum()")
     create_maps("nvi", "RFormatSubsetting",    "cs", "<Cmd>lua require('r.format.brackets').formatsubsetting()")
-    create_maps("nvi", "RInsertRoxygen",    "ir", "<Cmd>lua require('r.roxygen').insert_roxygen()")
 end
 
 local send = function(file_type)
