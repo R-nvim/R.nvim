eae389a855c1165540443764ae7a4144ebbdbf0d
Jakson Alves de Aquino <jalvesaq@gmail.com>
2024-10-29 20:24:38 -0300

    Fix completion when function has no arguments Close #235

--- lua/r/config.lua
+++ lua/r/config.lua
@@ -353,7 +353,7 @@ local do_common_global = function()
     -- Create or update the README (objls_ files will be regenerated if older than
     -- the README).
     local need_readme = false
-    local first_line = "Last change in this file: 2024-08-15"
+    local first_line = "Last change in this file: 2024-10-29"
     if
         vim.fn.filereadable(config.compldir .. "/README") == 0
         or vim.fn.readfile(config.compldir .. "/README")[1] ~= first_line
