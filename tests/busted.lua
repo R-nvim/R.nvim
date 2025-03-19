381808af6ee6ec6d3de40308777d5952d1a29c25
Jakson Alves de Aquino <jalvesaq@gmail.com>
2025-03-18 22:06:23 -0300

    Fix Selene warning

--- tests/busted.lua
+++ tests/busted.lua
@@ -14,16 +14,11 @@ require("lazy.minit").busted({
         "williamboman/mason-lspconfig.nvim",
         "williamboman/mason.nvim",
         {
-            "nvim-treesitter/nvim-treesitter",
+            [1] = "nvim-treesitter/nvim-treesitter",
             config = function()
                 require("nvim-treesitter.configs").setup({
                     sync_install = true,
-                    ensure_installed = {
-                        "latex",
-                        "r",
-                        "rnoweb",
-                        "yaml",
-                    },
+                    ensure_installed = { "latex", "r", "rnoweb", "yaml" },
                 })
             end,
         },
