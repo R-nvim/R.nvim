0865c86cc28014a871a59a91552a4c453418d479
Philippe Massicotte <pmassicotte@hotmail.com>
2025-03-19 15:11:58 -0400

    test(move_chunk_cursor_spec.lua): correct typo in test description for clarity

--- tests/move_chunk_cursor_spec.lua
+++ tests/move_chunk_cursor_spec.lua
@@ -2,7 +2,7 @@ local assert = require("luassert")
 local test_utils = require("./test_utils")
 
 describe("Chunk motion", function()
-    it("Move the cursor to the next_chunk, skipping eval: false chunk", function()
+    it("Move the cursor to the next chunk, skipping eval: false chunk", function()
         local bufnr = test_utils.create_r_buffer_from_file("tests/fixtures/chunks.qmd")
 
         vim.api.nvim_set_current_buf(bufnr)
