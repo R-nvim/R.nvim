cee69630868143c759702c1eb3a1b3afecb2c26e
Philippe Massicotte <pmassicotte@hotmail.com>
2025-09-25 14:17:17 -0400

    refactor(yaml): centralize YAML LSP setup for Quarto files into a dedicated module

--- ftplugin/yaml_rnvim.lua
+++ ftplugin/yaml_rnvim.lua
@@ -2,9 +2,5 @@
 local filename = vim.fn.expand("%:t")
 
 if filename == "_quarto.yml" or filename == "_quarto.yaml" then
-    -- Load the quarto configuration
-    dofile(
-        vim.fn.fnamemodify(debug.getinfo(1, "S").source:sub(2), ":h")
-            .. "/quarto_rnvim.lua"
-    )
+    require("r.yaml").setup()
 end
