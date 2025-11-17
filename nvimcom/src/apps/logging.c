4684d7b47413fd9fbca39630ab1dfb17ba5426af
Jakson Alves de Aquino <jalvesaq@gmail.com>
2025-11-17 15:00:40 -0300

    Enable logging

--- nvimcom/src/apps/logging.c
+++ nvimcom/src/apps/logging.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h> // For va_list
+#include <string.h>
 #include <time.h>
 
 #include "logging.h"
+
+static char *fnm;
 
 /**
  * @brief Logs a formatted message to a specified log file.
@@ -10,11 +14,10 @@
  * @param ... Variable arguments providing data to format.
  * @note This function is conditionally compiled only if Debug_NRS is defined.
  */
-
 __attribute__((format(printf, 1, 2))) void Log(const char *fmt, ...) {
 #ifdef Debug_NRS
     va_list argptr;
-    FILE *f = fopen("/dev/shm/rnvimserver_log", "a");
+    FILE *f = fopen(fnm, "a");
     if (!f)
         return;
     va_start(argptr, fmt);
@@ -27,9 +30,16 @@ __attribute__((format(printf, 1, 2))) void Log(const char *fmt, ...) {
 
 #ifdef Debug_NRS
 void init_logging(void) {
+#ifdef WIN32
+    fnm = malloc((strlen(getenv("TMP")) + 16) * sizeof(char));
+    sprintf(fnm, "%s/rnvimserver_log", getenv("TMP"));
+#else
+    fnm = malloc(sizeof(char) * 32);
+    strcpy(fnm, "/dev/shm/rnvimserver_log");
+#endif
     time_t t;
     time(&t);
-    FILE *f = fopen("/dev/shm/rnvimserver_log", "w");
+    FILE *f = fopen(fnm, "w");
     if (!f)
         return;
     fprintf(f, "NSERVER LOG | %s\n\n", ctime(&t));
--- nvimcom/src/apps/logging.h
+++ nvimcom/src/apps/logging.h
@@ -2,7 +2,7 @@
 #define LOGGING_H
 
 // Delete the trailing underline to enable logging
-#define Debug_NRS_
+#define Debug_NRS
 __attribute__((format(printf, 1, 2))) void Log(const char *fmt, ...);
 #ifdef Debug_NRS
 void init_logging(void);
