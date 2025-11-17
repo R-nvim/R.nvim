84049ce8f8cd8f1462a0c05939356d89218ef416
Jakson Alves de Aquino <jalvesaq@gmail.com>
2025-11-17 15:06:17 -0300

    Disable logging

--- nvimcom/src/apps/logging.h
+++ nvimcom/src/apps/logging.h
@@ -2,7 +2,7 @@
 #define LOGGING_H
 
 // Delete the trailing underline to enable logging
-#define Debug_NRS
+#define Debug_NRS_
 __attribute__((format(printf, 1, 2))) void Log(const char *fmt, ...);
 #ifdef Debug_NRS
 void init_logging(void);
