39b76df7bc1b3bbc9f8665735cbf0518241c1b53
Jakson Alves de Aquino <jalvesaq@gmail.com>
2025-12-07 08:43:55 -0300

    Stop searching function when line has no indentation

--- lua/r/lsp/init.lua
+++ lua/r/lsp/init.lua
@@ -149,7 +149,6 @@ local get_first_obj = function(line, lnum)
     local cp
     cp = string.byte(")")
     local idx
-    local ncol1 = 0
     repeat
         idx = #line
         while idx > 0 do
@@ -172,11 +171,11 @@ local get_first_obj = function(line, lnum)
             idx = idx - 1
         end
         if funname then break end
+        if line:find("^%S") then break end
         lnum = lnum - 1
         if lnum == 0 then break end
         line = vim.api.nvim_buf_get_lines(0, lnum - 1, lnum, true)[1]
-        if line:find("^%S") then ncol1 = ncol1 + 1 end
-    until ncol1 == 2
+    until true
     return pkg, funname, firstobj, line, lnum, idx
 end
 
